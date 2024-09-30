0b0eb1ec076905d388177294dd510f3b9d243dea
juneylvo <jjune.lo@gmail.com>
2024-09-30 12:20:13 -0400

    deleted 3a to dos

--- src/main/java/usecase/GetAverageGradeUseCase.java
+++ src/main/java/usecase/GetAverageGradeUseCase.java
@@ -27,7 +27,7 @@ public final class GetAverageGradeUseCase {
         final Team team = gradeDataBase.getMyTeam();
         // Call the API to get all the grades for the course for all your team members
         // TODO Task 3a: Complete the logic of calculating the average course grade for
-        //              your team members. Hint: the getGrades method might be useful.
+        // your team members. Hint: the getGrades method might be useful.
 
         if (count == 0) {
             return 0;
